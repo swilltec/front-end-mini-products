f0fdc5cf2b3576473779a6767572b6c7bba4d134
swill <swilltec@gmail.com>
2021-03-01 21:45:03 -0500

    styling form

---
+++ styling-form/script.js
@@ -0,0 +1,9 @@
+const  labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerHTML
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 100}ms">${letter}</span>`)
+        .join('')
+
+})
